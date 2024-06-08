1232312896a00549a2825782541e0ccc0d8a60f7
sulisak <sulisakvns23@gmail.com>
2024-06-08 23:55:52 +0700

    first commit

---
+++ application/views/layout/web/footer.php
@@ -0,0 +1,40 @@
+
+
+<div class="col-md-12 col-sm-12 col-xs-12 text-center" style="color: #fff;">
+
+
+<!-- 	Language <a style="color: #fff;" href="<?php echo $base_url;?>/?lang=th">ภาษาไทย</a> 
+	| <a style="color: #fff;" href="<?php echo $base_url;?>/?lang=lao">ພາສາລາວ</a>
+<br /> -->
+
+<center>
+<span style="color: #fff;">
+
+<br />
+
+
+<a href="https://www.facebook.com/%E0%BA%A5%E0%BA%B2%E0%BA%A7%E0%BA%8A%E0%BA%AD%E0%BA%9A%E0%BB%81%E0%BA%A7-Lao-Software-1703809623280832" target="_blank" 
+style="color:#f5eeee;">
+<img src="<?php echo $base_url;?>/pic/logo.jpg" 
+style="width:30px;box-shadow: 3px 3px rgba(0,0,0,.3);" 
+class="img-circle"> 
+
+<span style="text-shadow: 2px 2px 4px #000000;font-size:12px;">
+POS SYSTEM</span>
+</a> 
+
+</span>
+<br /> 
+<br />
+</center>
+
+</div>
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ expirenotice.php
@@ -0,0 +1,14 @@
+<?php
+
+	echo '<span style="font-weight: bold; color: green;"><font style="font-family:Phetsarath OT;">ໂປຼແກຼມໝົດສັນຍາແລ້ວ...ກະລຸນາຕໍ່ສັນຍາໃໝ່ </font></span>';
+
+echo '<br>';
+
+echo '<span style="font-weight: bold; color: orange;"><font style="font-family:Phetsarath OT;">ກະລຸນາຕິດຕໍ່ທີ່ເບີ 020 59339954</font></span>';
+
+					
+?>
+
+
+			
+	
